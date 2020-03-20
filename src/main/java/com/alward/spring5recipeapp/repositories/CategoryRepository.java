a30ab6d863c8a3aee37e633a9cbb8d303763ecbc
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-20 22:05:55 +0100

    Added SpringData Repositories

---
+++ src/main/java/com/alward/spring5recipeapp/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.alward.spring5recipeapp.repositories;
+
+import com.alward.spring5recipeapp.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
---
+++ src/main/java/com/alward/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.alward.spring5recipeapp.repositories;
+
+import com.alward.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
