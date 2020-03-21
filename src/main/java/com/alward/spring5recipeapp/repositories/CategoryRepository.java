53e5c67d90f3a6d3a1b98dfc6b375cc3c6b0ba67
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-03-21 14:35:28 +0100

    Added logic to display list of recipes

--- src/main/java/com/alward/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/alward/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.alward.spring5recipeapp.repositories;
 import com.alward.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/alward/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/alward/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.alward.spring5recipeapp.repositories;
 import com.alward.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
