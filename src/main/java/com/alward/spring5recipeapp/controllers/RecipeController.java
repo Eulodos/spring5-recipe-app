a10685e26e946186b4bed78ff7a8a228caa85b82
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2020-04-09 13:57:31 +0200

    Add initial functionality to add a new recipe

--- src/main/java/com/alward/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/alward/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.alward.spring5recipeapp.controllers;
 
+import com.alward.spring5recipeapp.commands.RecipeCommand;
 import com.alward.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+
+    @PostMapping("/recipe")
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
